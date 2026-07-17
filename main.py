fix: freetype bitmap rasterize overflow, use openCV to manually create the bitmap

main.py
@@ -5,6 +5,7 @@ import numpy as np
 from tqdm import tqdm
 
 from scripts.rasterize import rasterize
+from scripts.utils import iter_glyph, load_glyph
 
 root_dir = Path(__file__).parent
 
@@ -12,33 +13,6 @@ from argparse import ArgumentParser
 
 from PIL import Image
 
-CJK_RANGES = [
-    (0x4E00, 0x9FFF),   # CJK统一汉字
-    (0x3400, 0x4DBF),   # CJK扩展A
-    (0x20000, 0x2A6DF), # CJK扩展B
-    (0x2A700, 0x2B73F), # CJK扩展C
-    (0x2B740, 0x2B81F), # CJK扩展D
-    (0x2B820, 0x2CEAF), # CJK扩展E
-    (0x2CEB0, 0x2EBEF), # CJK扩展F
-    (0x30000, 0x3134F), # CJK扩展G
-    (0x31350, 0x323AF), # CJK扩展H
-    (0x2F800, 0x2FA1F), # CJK兼容汉字
-    (0xFA70, 0xFAD9),   # CJK兼容汉字
-    (0xF900, 0xFAFF),   # CJK兼容汉字
-]
-
-def load_glyph(face: freetype.Face, source: str):
-    try:
-        face.load_char(source, freetype.FT_LOAD_RENDER | freetype.FT_LOAD_NO_HINTING)
-        bmp = face.glyph.bitmap
-        if bmp.buffer is None or bmp.rows == 0 or bmp.width == 0:
-            return None
-        glyph = np.array(bmp.buffer).reshape(bmp.rows, bmp.width) > 0
-        return glyph
-        
-    except Exception as e:
-        print(f"'{e}'字不存在，跳过\n")
-        return None
 
 def rasterize_font(font, char, plot):
     rasterized_glyphs = {}
@@ -54,13 +28,10 @@ def rasterize_font(font, char, plot):
                 rasterized_glyphs[char] = rasterized_glyph
     else:
         # rasterize all chars in the font
-        for ranges in CJK_RANGES:
-            for code in tqdm(range(ranges[0], ranges[1]+1), total=ranges[1]-ranges[0]+1):
-                char = chr(code)
-                glyph = load_glyph(face, char)
-                if glyph is not None:
-                    rasterized_glyph = rasterize(glyph, plot=False)
-                    rasterized_glyphs[char] = rasterized_glyph
+        for glyph, char in iter_glyph(font_path):
+            if glyph is not None:
+                rasterized_glyph = rasterize(glyph, plot=False)
+                rasterized_glyphs[char] = rasterized_glyph
 
     return rasterized_glyphs
 

scripts/calibrate.py
@@ -69,12 +69,14 @@ def compute_gcd_stage1(edges):
     for dx, dy in edges.values():
         ds.extend([dx, dy])
     ds = np.asarray(ds)
+    ds = np.abs(ds)
     v_s, n_s = np.unique(ds[ds != 0], return_counts=True)
     gcd_candidate = v_s[n_s.argmax()]
     return gcd_candidate, v_s, n_s
 
 def compute_gcd_stage2(gcd_candidate, v_s, n_s, search_range=5):
-    gcds = np.arange(gcd_candidate // 2 - search_range, gcd_candidate // 2 + search_range + 1)
+    gcd_min = max(1, gcd_candidate - search_range)
+    gcds = np.arange(gcd_min, gcd_candidate + search_range + 1)
     loss = np.zeros_like(gcds)
     for i, d in enumerate(gcds):
         loss[i] = np.sum(np.abs(smod(v_s, d)) * n_s)
@@ -82,19 +84,25 @@ def compute_gcd_stage2(gcd_candidate, v_s, n_s, search_range=5):
     return gcd, loss
 
 def calibrate(points, edges, gcd):
+    """
+    将点坐标和边长校准到公度量的整数倍，即以公度值gcd为步长的网格上
+    """
     calibrated_edges = {}
     for pair, edge in edges.items():
         dx, dy = edge
         calibrated_edges[pair] = (sdivide(dx, gcd), sdivide(dy, gcd))
 
-    for pair, edge in edges.items():
-        print(pair, edge, calibrated_edges[pair])
     # coordinate calibration
-    offset = points[0]
-    calibrated_points = [(0, 0)]
+    # calibrate point 0, get the offset
+    x, y = points[0]
+    x_ = sdivide(x, gcd)
+    y_ = sdivide(y, gcd)
+    offset = (x - x_, y - y_)
+    calibrated_points = [(x_, y_)]
     is_start_of_contour = False
     for pair, edge in calibrated_edges.items():
         i, j = pair
+        # if i is the start of a contour, we need to minus the offset first, then calibrate it
         if is_start_of_contour:
             x, y = points[i]
             x_ = sdivide(x - offset[0], gcd)
@@ -117,16 +125,18 @@ def calibrate_font(outline: freetype.Outline, plot=False):
     edges = compute_edges(points, contours)
     gcd_candidate, v_s, n_s = compute_gcd_stage1(edges)
     gcd_candidate = gcd_candidate // 2 # 考虑公度量的一半，因为斜线笔画可能采用其一半
+    print(gcd_candidate)
     gcd, loss = compute_gcd_stage2(gcd_candidate, v_s, n_s)
+    print(gcd)
 
-    # 将点坐标和边长校准到公度量的整数倍，即以公度值gcd为步长的网格上
     calibrated_points = calibrate(points, edges, gcd)
 
-    # 可视化
-    fig, ax = plt.subplots(figsize=(8, 8))
-    draw(points, contours, color="gray", label_points=False, figure=(fig, ax))
-    draw(calibrated_points, contours, label_points=True, figure=(fig, ax))
-    plt.show()
+    if plot:
+        # 可视化
+        fig, ax = plt.subplots(figsize=(8, 8))
+        draw(points, contours, color="gray", label_points=False, figure=(fig, ax))
+        draw(calibrated_points, contours, label_points=True, figure=(fig, ax))
+        plt.show()
 
 def main(font, char, plot):
     face = freetype.Face(font)